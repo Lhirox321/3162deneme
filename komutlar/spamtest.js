dbb9bae18abac430db55afdf328eac0d29184698
Lhirox321 <46000888+Lhirox321@users.noreply.github.com>
2018-12-20 18:53:36 +0300

    Update spamtest.js

--- komutlar/spamtest.js
+++ komutlar/spamtest.js
@@ -1,41 +1,4 @@
-const Discord = require('discord.js');
 
-exports.run = (client, message, args) => {
-	let mesaj = args.slice(0).join(' ');
-	if (mesaj.length < 1) return message.reply('**K**');
-    const embed = new Discord.RichEmbed()
-    .setAuthor('')
-    .setColor(3447003)
-    .setDescription(`** ${mesaj} ` + message.author.username + ' **hacked**')
-    .addField('HACKED', 'BY HUSEYIN ALI')
-    .addField('HACKED', 'BY HUSEYIN ALI')
-    .addField('HACKED', 'BY HUSEYIN ALI')
-    .addField('HACKED', 'BY HUSEYIN ALI')
-    .addField('HACKED', 'BY HUSEYIN ALI')
-    .addField('HACKED', ' BY HUSEYIN ALI')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    .addField('HACKED', '@everyone')
-    return message.channel.sendEmbed(embed);
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
   permLevel: 0
 };
 
