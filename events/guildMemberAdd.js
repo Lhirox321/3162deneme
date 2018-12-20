396f36867bc6f51f0b70d4eb8919ce963428959a
3162deneme123 <46000888+Lhirox321@users.noreply.github.com>
2018-12-20 18:56:44 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,2 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Hoş geldin **' + username + '**!');
-    member.guild.defaultChannel.send('hg '+username+'');
+
 };
