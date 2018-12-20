be90fe681a8f0fc0bef8319c061debb1c0c04b76
Lhirox321 <46000888+Lhirox321@users.noreply.github.com>
2018-12-20 18:48:30 +0300

    Update bravery.js

--- komutlar/bravery.js
+++ komutlar/bravery.js
@@ -1,17 +1,4 @@
-const Discord = require('discord.js')
 
-exports.run = (client, message) => {
-    const embed = new Discord.RichEmbed()
-    .setImage(`http://18.184.116.22/api/cerceve?cerceve=bravery2&url=${message.author.avatarURL}`)
-    .setColor(0xff7f00)
-    return message.channel.send(embed)
- };
- 
-  exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 0
   };
  
   exports.help = {
